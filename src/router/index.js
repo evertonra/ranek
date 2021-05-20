5d732b27d8e57cd664f229ea2063addfc28ddd5c
Éverton <everton-adams@hotmail.com>
2021-05-20 13:01:36 -0300

    primeiro commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,36 @@
 import Vue from "vue"
-import VueRouter from "vue-router"
+import Router from "vue-router"
 import Home from "../views/Home.vue"
+import Produto from "../views/Produto.vue"
+import Login from "../views/Login.vue"
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-]
 
-const router = new VueRouter({
+
+export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/produto/:id",
+      name: "produto",
+      component: Produto,
+      props: true
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+  ],
+  scrollBehavior() {
+    return window.scrollTo({top: 0,  behavior: "smooth"})
+  }
 })
 
-export default router
