e0540c42251eebb8648f677d044436681ca60f9d
Éverton <everton-adams@hotmail.com>
2021-05-20 17:24:41 -0300

    fim aula vuex formulario

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ Vue.use(Vuex)
 
 
 export default new Vuex.Store({
+  strict: true,
   state: {
     login: false,
     usuario: {
@@ -26,7 +27,7 @@ export default new Vuex.Store({
       state.login = payload
     },
     UPDATE_USUARIO(state, payload) {
-      state.usuario = payload
+      state.usuario = Object.assign(state.usuario, payload)
     },
   },
   actions: {
